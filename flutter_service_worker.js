96a94e34facb4fe1a40e7bc48c42bd1337208f3b
Zenon Skuza <zenon@skuza.net>
2024-12-17 09:58:37 +1100

    v1.3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,14 +22,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "36a60659414f23781677840913178c09",
+"flutter_bootstrap.js": "605a6038c798cd9118e239eb1f834a85",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "6b14de10ebcfca5b6020199bc1fc3feb",
 "/": "6b14de10ebcfca5b6020199bc1fc3feb",
-"main.dart.js": "5335d21bbd5b0d824096bb27c5549c05",
+"main.dart.js": "7804059867d358602b6d0a10ae008530",
 "manifest.json": "d9f960d53f7a533ba83dde82bb9a108a",
 "version.json": "f3750ba2ab524cc05fad3b96b4b45d14"};
 // The application shell files that are downloaded before a service worker can
